053e3352097287b42febb38dc16da485b7731df3
onlydev7777 <onlydev7777@gmail.com>
2024-04-22 16:01:24 +0900

    1) apigateway-service    - eureka client 설정 추가    - gateway routes 설정      * uri: lb://FIRST-SERVICE             lb://SECOND-SERVICE     - gateway filters 설정       * GlobalFilter : 전체 적용 필터       * CustomFilter : 사용자 정의 필터       * LoggingFilter : 사용자 정의 순서 적용 필터(OrderedGatewayFilter)         => 데코레이터 패턴으로 구성되어 있음
    2) first-service
       - eureka client 설정 추가
       - 랜덤 포트 적용
         * 랜덤 포트 적용으로 인한 instance-id 설정 추가
           instance-id: ${spring.application.name}:${spring.application.insatnce_id:${random.value}}
    
    3) second-service
       - eureka client 설정 추가

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,38 @@
 package com.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/first-service")
+@RestController
 public class FirstServiceController {
+
+  private final Environment env;
+
   @GetMapping("/welcome")
-  public String welcome(){
+  public String welcome() {
     return "welcome to the First service!";
   }
 
+  @GetMapping("/message")
+  public String message(@RequestHeader("first-request") String header) {
+    log.info(header);
+    return "Hello World in First Service";
+  }
+
+  @GetMapping("/check")
+  public String check(HttpServletRequest request) {
+    log.info("server port = {}", request.getServerPort());
+
+    return String.format("Hi, there. THis is a messsage from First Service on Port %s"
+        , env.getProperty("local.server.port"));
+  }
 }
