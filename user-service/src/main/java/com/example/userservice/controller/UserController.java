0e396b7c2743e41ca35f0c7f4faab70bbc1ca026
onlydev7777 <onlydev7777@gmail.com>
2024-04-26 17:39:08 +0900

    1) user-service
       - application.yml
         : actuator 관련 설정 추가
    
       - bootstrap.yml
         : Spring Config Server 설정 추가

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -35,8 +35,13 @@ public class UserController {
 
   @GetMapping("/health-check")
   public String status() {
-    return String.format("It's Working in User Service on Port %s",
-        env.getProperty("local.server.port"));
+    return String.format("It's Working in User Service"
+        + ", port(local.server.port)=" + env.getProperty("local.server.port")
+        + ", port(server.port)=" + env.getProperty("server.port")
+        + ", gateway ip(env)=" + env.getProperty("gateway.ip")
+        + ", message=" + env.getProperty("greeting.message")
+        + ", token secret=" + env.getProperty("token.secret")
+        + ", token expiration time=" + env.getProperty("token.expiration_time"));
   }
 
   @GetMapping("/welcome")
