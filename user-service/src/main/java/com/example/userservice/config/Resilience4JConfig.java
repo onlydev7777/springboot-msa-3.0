743c8829ccb8f133625921c34be73a8cd584abda
onlydev7777 <onlydev7777@gmail.com>
2024-05-08 17:20:38 +0900

    feat: 1) 장애처리와 Microservice 분산 추적.md 파일 작성

--- user-service/src/main/java/com/example/userservice/config/Resilience4JConfig.java
+++ user-service/src/main/java/com/example/userservice/config/Resilience4JConfig.java
@@ -15,12 +15,14 @@ public class Resilience4JConfig {
   @Bean
   public Customizer<Resilience4JCircuitBreakerFactory> globalCustomConfiguration() {
     CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
-        .failureRateThreshold(4)
-        .waitDurationInOpenState(Duration.ofSeconds(4))
+        .failureRateThreshold(4)  //실패 임계치 % 설정
+        .waitDurationInOpenState(Duration.ofSeconds(4)) //써킷 open 지속 시간
+        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)  //카운트 or 시간 기반으로 설정할 수 있음
+        .slidingWindowSize(2)   // 카운트일 경우 사이즈 만큼의 마지막 요청 횟수로 실패 임계치 계산, 시간일 경우 사이즈 만큼의 seconds 로 call 집계해서 실패 임계치 계산
         .build();
 
     TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
-        .timeoutDuration(Duration.ofSeconds(4))
+        .timeoutDuration(Duration.ofSeconds(4)) //supplier 서비스(order-service)의 타임아웃 오류 발생 기준 seconds
         .build();
 
     return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
