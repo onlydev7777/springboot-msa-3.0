444a1af1e98c4fed05fd5c463fe0c47459c0f5a0
onlydev7777 <onlydev7777@gmail.com>
2024-05-14 17:40:21 +0900

    feat) 테이블명 변경

--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into catalog(product_id, product_name, stock, unit_price, created_at)
+insert into catalogs(product_id, product_name, stock, unit_price, created_at)
 values ('CATALOG-001', 'Berlin', 100, 1500, CURRENT_TIMESTAMP);
-insert into catalog(product_id, product_name, stock, unit_price, created_at)
+insert into catalogs(product_id, product_name, stock, unit_price, created_at)
 values ('CATALOG-002', 'Tokyo', 110, 1000, CURRENT_TIMESTAMP);
-insert into catalog(product_id, product_name, stock, unit_price, created_at)
+insert into catalogs(product_id, product_name, stock, unit_price, created_at)
 values ('CATALOG-003', 'Stockholm', 120, 2000, CURRENT_TIMESTAMP);
