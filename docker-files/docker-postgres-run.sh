3d6f5ab614604fa8d4edda90187901866f47ae95
onlydev7777 <onlydev7777@gmail.com>
2024-05-16 17:49:29 +0900

    feat) - 어플리케이션 배포를 위한 컨테이너 가상화.md 파일 작성 - 어플리케이션 배포 - Docker Container.md 파일 작성

--- docker-files/docker-postgres-run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -p 54399:54316 --name postgresdb --network ecommerce-network yungyeongjun/my-postgres:0.0.1
--- docker-files/docker-rabbitmq-run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --name rabbitmq --network ecommerce-network -p 15672:15672 -p 5672:5672 -p 15671:15671 -p 4369:4369 -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest rabbitmq:management
